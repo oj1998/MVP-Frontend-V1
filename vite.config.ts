690e1b4b3df749673ff3f81390fafea112623527
oj1998 <oliverwjimenez@gmail.com>
2025-02-08 17:50:04 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    root: '.',
+  }
 });
